9829a9396debd63a5569ace0f03decb0dfe8b3dd
Marcel Schoombee <marcelsch01@yahoo.com>
2024-05-30 19:39:12 +0200

    Started on header

---
+++ src/components/header.tsx
@@ -0,0 +1,18 @@
+function Header() {
+    return (
+        <>
+        <div>
+            <div className="header--div">
+                <nav>
+                    <img src="./assets/DesignImage.png" alt="Logo Image" className="header--img" />
+                    <h2>Marcel Schoombee Developer</h2>
+                </nav>
+            </div>
+
+        </div>
+        </>
+    )
+
+}
+
+export default Header
