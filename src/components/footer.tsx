1091399b77f7a6901b163e8b4d43a22490ef77a6
Marcel Schoombee <marcelsch01@yahoo.com>
2024-05-30 21:30:06 +0200

    footer finished

--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -1,8 +1,10 @@
-export default function Footer() {
+ export default function Footer() {
     return (
         <div>
         <footer className="footer">
-            <h4></h4>
+            <h4>Contact details:</h4>
+            <p>Tel: 069 231 2917</p>
+            <p>email: marcelsch01@yahoo.com</p>
         </footer>
         </div>
     )
